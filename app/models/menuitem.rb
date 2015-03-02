c09a2a0e8ec3a904a2ffe838c9a2129b18ff0100
pascbjumper2 <stowers.joshua@live.com>
2015-03-02 11:48:12 -0600

    Website for the Classic Garage

--- app/models/menuitem.rb
+++ app/models/menuitem.rb
@@ -1,3 +1,4 @@
 class Menuitem < ActiveRecord::Base
-
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
--- db/migrate/20150302173137_add_images_to_menuitems.rb
+++ db/migrate/20150302173137_add_images_to_menuitems.rb
@@ -1,4 +1,8 @@
 class AddImagesToMenuitems < ActiveRecord::Migration
   def change
+    add_column :menuitems, :image_file_name, :string
+    add_column :menuitems, :image_content_type, :string
+    add_column :menuitems, :image_file_size, :integer
+    add_column :menuitems, :image_updated_at, :datetime
   end
 end

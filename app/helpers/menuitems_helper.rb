fd6d1e53cb06344f13a3c019f63b2783260444f0
pascbjumper2 <stowers.joshua@live.com>
2015-03-11 10:15:20 -0500

    Changed all instances of "Entre" to "Entree" Derp

--- app/helpers/menuitems_helper.rb
+++ app/helpers/menuitems_helper.rb
@@ -1,6 +1,6 @@
 module MenuitemsHelper
   def catcount_to_cat(catcount)
-    hash = { 1 => "Entre", 2 => "Side", 3 => "Dessert", 4 => "Appetizer", 5 => "Other" }
+    hash = { 1 => "Entree", 2 => "Side", 3 => "Dessert", 4 => "Appetizer", 5 => "Other" }
     hash[catcount]
   end
 end

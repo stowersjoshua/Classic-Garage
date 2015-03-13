1665a8b3df33996715dd3b6f060c2129c4293990
pascbjumper2 <stowers.joshua@live.com>
2015-03-12 19:03:19 -0500

    Pluralized catergory names via catcount_to_cat helper method in menuitems helper

--- app/helpers/menuitems_helper.rb
+++ app/helpers/menuitems_helper.rb
@@ -1,6 +1,6 @@
 module MenuitemsHelper
   def catcount_to_cat(catcount)
-    hash = { 1 => "Entree", 2 => "Side", 3 => "Dessert", 4 => "Appetizer", 5 => "Other" }
+    hash = { 1 => "Entrees", 2 => "Sides", 3 => "Desserts", 4 => "Appetizers", 5 => "Other" }
     hash[catcount]
   end
 end
